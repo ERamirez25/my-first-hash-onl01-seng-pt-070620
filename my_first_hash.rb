83cf0564a5ac9aa8730fdf9fa6b8614916143d36
Elaine Ramirez <elaineramirez208@yahoo.com>
2020-07-15 03:07:40 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -5,11 +5,12 @@ end
 
 
 def shipping_manifest
-  shipping_manifest = 
+shipping_manifest = {
+ "whale bone corsets" => 5, 
+ "porcelain vases" => 2, 
+ "oil paintings" => 3
+ }
   
-
-       
-
 end
 
 def retrieval
